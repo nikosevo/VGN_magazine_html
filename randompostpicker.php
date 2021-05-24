95b7979520e31a50229a79b740d790a47c8071be
balantis <balantispat@gmail.com>
2021-05-24 15:44:54 +0300

    random article picker

--- randompostpicker.php
+++ randompostpicker.php
@@ -1,23 +1,8 @@
 
 <?php 
-include "common.php";
-function pickapost() {  
-    $link = mysqli_connect("localhost","root","","database");
-    if ($link===false){
-        die("ERROR: Den egine sindesi sthn vasi");
-    }
-  session_start(); 
-  $sql = "SELECT COUNT(postID) FROM posts";
-  $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-  $data = mysqli_fetch_assoc($result);  
-  $id = rand(1,10);
-  echo '<script>postpicker(<?php echo json_encode($id); ?>);</script>';}
-pickapost();
+   
+    $id = rand(1,10);
+    header("LOCATION: Post.php?uid=$id");
+    exit();
 
 ?>
-<script>
-       function postpicker(id) {
-           
-        window.location.href="Post.php?uid=" + $id;}
-           
-    </script>
